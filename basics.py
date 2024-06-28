4fb5ceca6ffa6c5b54500c56ebb4009864c1d35d
13mir <13mironov@gmail.com>
2024-06-28 04:10:44 +0300

    Add something new

---
+++ basics.py
@@ -0,0 +1,19 @@
+print('Hi there!!!')
+msg = 'Hi there!!!'
+print(msg)
+First_name = 'Gvido'
+Last_name = 'Van Rossum'
+msg=f'Hi, {First_name} {Last_name}'
+print(msg)
+string_lenght = len('Hi there!!!')
+print(string_lenght)
+a = 'Hi!' .upper()
+print(a)
+b = 'Hi!' .lower()
+print(b)
+c = 'Hi!' .capitalize()
+print(c)
+e = 'Hi theare' .replace('a','')
+print(e)
+f = 'Hi there' .replace('there','python')
+print(f)
